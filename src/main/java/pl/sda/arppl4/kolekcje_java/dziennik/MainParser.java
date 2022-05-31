e1010bf5caa54df4943a2f1e68f8dd46a94024aa
Paweł Recław <preclaw@purestorage.com>
2022-05-31 20:28:09 +0200

    Koniec parser university

---
+++ src/main/java/pl/sda/arppl4/kolekcje_java/dziennik/MainParser.java
@@ -0,0 +1,13 @@
+package pl.sda.arppl4.kolekcje_java.dziennik;
+
+import java.util.Scanner;
+
+public class MainParser {
+    public static void main(String[] args) {
+        Dziennik dziennik = new Dziennik();
+        Scanner scanner = new Scanner(System.in);
+
+        ParserUniversity parserUniversity = new ParserUniversity(dziennik, scanner);
+        parserUniversity.pracuj();
+    }
+}
